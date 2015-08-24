6709677ab971c1eae04e5f9f24df823ae22d19ca
hubenlv <bhu@hp.com>
2015-08-24 23:33:38 +0800

    demo

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,12 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngResource', 'starter.controllers', 'starter.services','d3', 'starter.directives'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
   });
@@ -25,62 +14,35 @@ angular.module('starter', ['ionic', 'ngResource', 'starter.controllers', 'starte
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
-
-  // setup an abstract state for the tabs directive
-    /*.state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })*/
-
-  // Each tab has its own nav history stack:
 
   .state('dash', {
     url: '/dash',
     views: {
       'dash': {
         templateUrl: 'templates/dash.html',
-        controller: 'DashCtrl'
+        controller: 'DemoCtrl'
       }
     }
   })
-
-  .state('org', {
-      url: '/org',
+  .state('kpi', {
+      url: '/kpi',
       views: {
         'dash': {
-          templateUrl: 'templates/org.html',
-          controller: 'OrgCtrl'
+          templateUrl: 'templates/bz-KPI.html',
+          controller: 'DemoCtrl'
         }
       }
     })
-  
-    .state('attend', {
-      url: '/attend',
+  .state('green-cross', {
+      url: '/green-cross',
       views: {
         'dash': {
-          templateUrl: 'templates/kao-qin.html',
-          controller: 'AttendCtrl'
+          templateUrl: 'templates/green-cross.html',
+          controller: 'DemoCtrl'
         }
       }
-    })
-/*
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });*/
-
-  // if none of the above states are matched, use this as the fallback
+    })  
   $urlRouterProvider.otherwise('/dash');
 
 });
