3358975162b55cb37e00993d11c7a4d263900565
hubenlv <bhu@hp.com>
2015-08-27 09:09:35 +0800

    zoom test

--- www/js/module.directive.js
+++ www/js/module.directive.js
@@ -69,6 +69,7 @@ angular.module('starter.directives', [])
           });
         }
         $scope.$on('$ionicView.leave', function(){
+          console.log('...');
           $elm.html('');
         })
       }
