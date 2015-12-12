d3a55dfccbff983b61a7edf347f2d0485b6535e3
benlv.hu <benlv.hu@dianrong.com>
2015-12-12 18:40:29 +0800

    Garden Manual Switch

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -864,19 +864,46 @@ angular.module('starter.controllers', [])
   });
 
   $scope.myInterval = 5000;
-    $scope.noWrapSlides = false;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
-      var newWidth = 600 + slides.length + 1;
-      slides.push({
-        image: '//placekitten.com/' + newWidth + '/300',
-        text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-          ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-      });
-    };
-    for (var i=0; i<4; i++) {
-      $scope.addSlide();
+  $scope.noWrapSlides = false;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+
+  $scope.next = function(){
+    for(var i=0, len = $scope.slides.length;i<len;i++){
+      if($scope.slides[i].active){
+        $scope.slides[i].active = false;
+        if(i+1>=len){
+          i=0;
+        }else{
+          i++;
+        }
+        $scope.slides[i].active = true;
+      }
     }
+  }
+  $scope.prev = function(){
+    for(var i=0, len = $scope.slides.length;i<len;i++){
+      if($scope.slides[i].active){
+        $scope.slides[i].active = false;
+        if(i-1<0){
+          i=len-1;
+        }else{
+          i--;
+        }
+        $scope.slides[i].active = true;
+      }
+    }
+  }
 
 }])
 .controller('LineKPICtrl', ['$scope', 'Constant', '$state', 'localStorageService', 'KPIDetail',
