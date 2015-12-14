1b17199747236bfb95a88aeaf5fefae5c9ffeedf
benlv.hu <benlv.hu@dianrong.com>
2015-12-14 22:01:54 +0800

    Garden Read Image List

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -885,6 +885,46 @@ angular.module('starter.controllers', [])
 
   $scope.$on('$ionicView.enter', function(e) {
     $scope.selectedCriteria = localStorageService.get('criteria');
+    $scope.msg = '';
+    var imagePath = Constant.getImagePath();
+    if(imagePath && !imagePath.nativeURL){
+      window.resolveLocalFileSystemURL(Constant.getImagePath().nativeURL, function(filesystem){
+          // we must pass what the PhoneGap API doc examples call an "entry" to the reader
+          // which appears to take the form constructed below.
+          var directoryReader = filesystem.createReader();
+          // Get a list of all the entries in the directory
+          $scope.loading = Constant.loading;
+          directoryReader.readEntries(function(entries){
+            $scope.loading = '';
+            $scope.msg += '目录列表遍历中...';
+            if(!entries){
+              $scope.msg += '目录列表为空';
+              return;
+            }
+            // again, Eclipse doesn't allow object inspection, thus the stringify
+            $scope.images = entries.filter(function(entry){
+              return entry.name.indexOf('.') != 0 && entry.isFile && Constant.isExtSupport(entry.name);
+            }).sort(function(a,b){
+              // alphabetically sort the entries based on the entry's name
+              return (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1);
+            })
+            $scope.msg += 'images============='+JSON.stringify($scope.images);
+            if(!$scope.images || !$scope.images.length){
+              
+            }
+            $scope.$apply();
+          },function(err){
+            $scope.loading = Constant.loadingError;
+            $scope.msg += 'requestFileSystemSuccess目录'+path+'失败:'+JSON.stringify(err);
+          });
+
+        },
+        function(err){
+          $scope.loading = Constant.loadingError;
+          $scope.msg += 'beginBrowseForFiles目录失败:'+JSON.stringify(err);
+        }
+      );
+    }
 
     $scope.myInterval = Constant.getInterval()*1000;
   });
