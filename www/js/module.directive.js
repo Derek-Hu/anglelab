8e249ddd4854932bd45c9354df7f7278eec14046
benlv.hu <benlv.hu@dianrong.com>
2016-11-12 20:02:30 +0800

    Remove Directive

--- www/js/module.directive.js
+++ www/js/module.directive.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 angular.module('starter.directives',['d3'])
   .directive('kaoQinChart', ['d3', '$window', function(d3, $window) {
 
@@ -432,70 +432,6 @@ angular.module('starter.directives',['d3'])
           }
       };
     })
-.directive('zoomable', ['$timeout',function($timeout) {
-  return {
-    restrict: 'A',
-    scope: true,
-    link: function($scope, $element, $attrs) {
-      var target = $element[0];
-
-      var styleTarget = target.children[0].children[0].children[0];
-      //console.log(target, target.children[0],styleTarget);
-      styleTarget.style.webkitTransition = 'all linear 0.05s';
-
-
-
-      touch.on(target, 'touchstart', function(ev){
-        ev.preventDefault();
-      });
-
-      var initialScale = 1;
-      var currentScale;
-
-      touch.on(target, 'pinchend', function(ev){
-        currentScale = ev.scale - 1;
-        currentScale = initialScale + currentScale;
-        currentScale = currentScale > 2 ? 2 : currentScale;
-        currentScale = currentScale < 0.5 ? 0.5 : currentScale;
-        styleTarget.style.webkitTransform = 'scale(' + currentScale + ')';
-      });
-      touch.on(target, 'pinchend', function(ev){
-        initialScale = currentScale;
-      });
-
-      var clicks = 0;
-      var lastClick = new Date();
-      $element.bind('click', function (evt) {
-          var dateDiff = new Date() - lastClick;
-          //console.log('dateDiff', dateDiff);
-          if (dateDiff > 300) { // 300 ms
-              clicks = 0;
-          }
-          lastClick = new Date();
-          clicks++;
-          //console.log('clicks'+clicks);
-          if (clicks == 1) {
-              $timeout(function () {
-                  if (clicks == 1) {
-                      //....
-                  } else {
-                      if(currentScale!== undefined && currentScale!==1){
-                        currentScale = 1;
-                        styleTarget.style.webkitTransform = 'scale(' + currentScale + ')';
-                      }else{
-                        if($attrs.onDoubleClick){
-                          $scope.$apply(function () {
-                              $scope.$eval($attrs.onDoubleClick);
-                          });
-                        }
-                      }
-                  }
-              }, 300);
-          }
-      });
-    }
-  };
-}])
 .directive('scrollArea', ['$window',function($window) {
   return {
     restrict: 'A',
