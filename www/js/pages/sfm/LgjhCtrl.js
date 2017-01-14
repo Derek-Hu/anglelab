5e95c24907206f947d95ac318e53f5466563f7e3
benlv <benlv.hu@dianrong.com>
2017-01-14 17:28:09 +0800

    轮岗计划数据结构调整

--- www/js/pages/sfm/LgjhCtrl.js
+++ www/js/pages/sfm/LgjhCtrl.js
@@ -16,9 +16,9 @@ var Controller = function (localStorageService, $scope, Backend, MetaDataSvc, $s
                 $scope.loadingStatus = '加载失败';
                 return;
             }
-            data.sort(function (a, b) {
+            /*data.sort(function (a, b) {
                 return parseInt(a.Order_number, 10) - parseInt(b.Order_number, 10);
-            });
+            });*/
             var rows = [];
             /*
               rows = [{
@@ -34,45 +34,42 @@ var Controller = function (localStorageService, $scope, Backend, MetaDataSvc, $s
             /*
               {
                 "Job_name":"检验",
-                "month_r":"11",
-                "Order_number":7,
-                "Operate_Name":"张四",
-                "Rotation_Name":"张五"
+                "Job_code":"11",
+                "month1":7,
+                ...
+                "month12": "23aa"
               }
             */
             for (var i = 0, len = data.length; i < len; i++) {
                 // find job type
                 var jobType = {};
                 var record = data[i];
+                record.months = [
+                    record.month1,
+                    record.month2,
+                    record.month3,
+                    record.month4,
+                    record.month5,
+                    record.month6,
+                    record.month7,
+                    record.month8,
+                    record.month9,
+                    record.month10,
+                    record.month11,
+                    record.month12
+                ];
                 for (var j = 0, jl = rows.length; j < jl; j++) {
                     if (rows[j] && rows[j].name == record.Job_name) {
                         jobType = rows[j];
+                        jobType.items.push(record);
                         break;
                     }
                 }
                 if (j == jl) {
                     jobType.name = record.Job_name;
-                    jobType.items = [];
+                    jobType.items = [record];
                     rows.push(jobType);
                 }
-                // find items
-                var months = [];
-                var items = {};
-                for (var k = 0, lk = jobType.items.length; k < lk; k++) {
-                    if (record.Operate_Name == jobType.items[k].operator &&
-                        record.Rotation_Name == jobType.items[k].rotate) {
-                        items = jobType.items[k];
-                        break;
-                    }
-                }
-                if (k == lk) {
-                    items.operator = record.Operate_Name;
-                    items.rotate = record.Rotation_Name;
-                    // 12 months
-                    items.months = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-                    jobType.items.push(items);
-                }
-                items.months[record.month_r - 1] = 1;
             }
             $scope.rows = rows;
             console.log(rows);
