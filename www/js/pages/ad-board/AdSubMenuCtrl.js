fcaa94f35c6f18ead932539c0052b16f839e6e70
benlv <benlv.hu@dianrong.com>
2017-08-08 19:14:55 +0800

    Style

--- www/js/pages/ad-board/AdSubMenuCtrl.js
+++ www/js/pages/ad-board/AdSubMenuCtrl.js
@@ -5,39 +5,45 @@ var Controller = function ($scope, localStorageService, $state) {
             off: {
                 name: '下架',
                 enName: 'off the shelf',
-                show: false
-            },
+                show: false,
+              },
             start: {
                 name: '上线',
                 enName: 'on-line',
-                show: false
-            },
+                show: false,
+              },
+            on: {
+                name: '上架',
+                enName: 'on the shelf',
+                show: false,
+              },
             pull: {
                 name: '拉动',
                 enName: 'kick-start',
-                show: false
-            },
+                show: false,
+              },
             member: {
                 name: '人员调整',
                 enName: 'turnover',
-                show: false
-            }
-        };
+                show: false,
+              },
+          };
         $scope.menus = Object.keys($scope.menuConfig);
         var loginUser = localStorageService.get('loginUser');
         var permssions, i, len;
 
         if (!loginUser) {
-            $state.go('ad-login');
-            return;
+          $state.go('ad-login');
+          return;
         }
+
         permssions = loginUser.permssionMap.AD;
 
         for (i = 0, len = permssions.length; i < len; i++) {
-            $scope.menuConfig[permssions[i]].show = true;
+          $scope.menuConfig[permssions[i]].show = true;
         }
-    });
+      });
 
-};
+  };
 
 module.exports = ['$scope', 'localStorageService', '$state', Controller];
