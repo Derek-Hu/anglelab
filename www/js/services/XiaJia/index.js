ebb741adb32d758ebd650d4d8a54c74ffde3f287
benlv <benlv.hu@dianrong.com>
2017-09-06 23:25:47 +0800

    Shangjia Interface

--- www/js/services/XiaJia/index.js
+++ www/js/services/XiaJia/index.js
@@ -66,7 +66,29 @@ module.exports = {
           success(function (data, status, headers) {
               if (data && data.respCode === 'success') {
                 deferred.resolve();
+              }else if(data && Object.prototype.toString.call(data) === '[object Array]'){
+                data = data.sort(function(a, b){return parseInt(a.orderNum) - parseInt(b.orderNum)});
+                deferred.resolve(data);
               } else {
+                deferred.reject((data && data.respCode) ? data.respCode : null);
+              }
+            }).
+          error(function (data, status, headers) {
+              deferred.reject(null);
+            });
+
+          return deferred.promise;
+        }
+
+        function shangjiaWithKuwei(params) {
+          var deferred = $q.defer();
+
+          /*eslint-disable*/
+          $http({ method: 'GET', url: Backend().shangjiaWithKuwei + params }).
+          success(function (data, status, headers) {
+              if (data && data.respCode === 'success') {
+                deferred.resolve();
+              }else {
                 deferred.reject((data && data.respCode) ? data.respCode : null);
               }
             }).
@@ -81,6 +103,7 @@ module.exports = {
             getList: getList,
             xiajia: xiajia,
             shangjia: shangjia,
+            shangjiaWithKuwei: shangjiaWithKuwei,
             getOnList: getOnList,
           };
       },],
