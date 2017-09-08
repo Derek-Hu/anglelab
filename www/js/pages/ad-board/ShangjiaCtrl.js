c3e36f9eb9cecb40133b0bfda52f41fd72ae3519
benlv <benlv.hu@dianrong.com>
2017-09-08 08:56:55 +0800

    Pop style

--- www/js/pages/ad-board/ShangjiaCtrl.js
+++ www/js/pages/ad-board/ShangjiaCtrl.js
@@ -1,4 +1,4 @@
-var Controller = function ($ionicActionSheet, $scope, XiaJia, localStorageService, $state, $ionicPopup, $rootScope, $interval) {
+var Controller = function ($ionicModal, $scope, XiaJia, localStorageService, $state, $ionicPopup, $rootScope, $interval) {
 
     var seconds = 185000;
     var timer = null;
@@ -12,6 +12,7 @@ var Controller = function ($ionicActionSheet, $scope, XiaJia, localStorageServic
 
         alertPopup.then(function () {});
       };
+      var alertPopup;
 
     function showSelectKuwei(kuweis, item){
       if(timer){
@@ -19,28 +20,57 @@ var Controller = function ($ionicActionSheet, $scope, XiaJia, localStorageServic
         timer = null;
       }
 
-      var hideSheet = $ionicActionSheet.show({
-         buttons: kuweis.map(function(kw){
-           return {text: kw.locCode+'('+kw.locId+')'}
-         }),
-         titleText: '选择零件'+item.itemCode+'上架库位',
-         cancelText: '取消',
-         cancel: function() {
-           if(!timer){
-             timer = $interval(function () {
-                 $scope.loadList();
-               }, seconds);
-           }
-        　},
-         buttonClicked: function(index) {
-           _shangjiaWithKuwei(kuweis[index].locId, item);
-           return true;
-         }
-       });
+      $scope._kuweis = kuweis;
+      $scope._selectedItme = item;
+      alertPopup = $ionicPopup.alert({
+          template: `
+            <div class="">
+              <div ng-repeat="k in _kuweis" ng-click="_shangjiaWithKuwei(k.locId, _selectedItme);" class="list card">
+                <div><div>
+                  <span class="first" ng-bind="k.locCode"></span>
+                  <span ng-bind="k.locId"></span>
+                </div></div>
+              </div>
+            </div>
+
+          `,
+          cssClass: 'shangjiaSelectKuwei',
+          title: '选择零件'+item.itemCode+'上架库位',
+          scope: $scope,
+          buttons: [
+            { text: '取消',type: 'button-positive',
+              onTap: function(e) {
+                item.txt = '上架';
+              }
+             }
+          ]
+        });
+
+      alertPopup.then(function () {});
+
+      // var hideSheet = $ionicModal.show({
+      //    buttons: kuweis.map(function(kw){
+      //      return {text: kw.locCode+'('+kw.locId+')'}
+      //    }),
+      //    titleText: '选择零件'+item.itemCode+'上架库位',
+      //    cancelText: '取消',
+      //    cancel: function() {
+      //      if(!timer){
+      //        timer = $interval(function () {
+      //            $scope.loadList();
+      //          }, seconds);
+      //      }
+      //   　},
+      //    buttonClicked: function(index) {
+      //      _shangjiaWithKuwei(kuweis[index].locId, item);
+      //      return true;
+      //    }
+      //  });
     }
     function _shangjiaWithKuwei(locId, item){
       XiaJia.shangjiaWithKuwei('?shiftId=' + item.id + '&userName=' + $scope.loginUser.loginNme + '&locId=' + locId).then(function () {
             $scope.showAlert('上架成功', true);
+            alertPopup&&alertPopup.close();
             $scope.loadList();
         }).catch(function (errorMsg) {
           $scope.showAlert('上架失败', false, errorMsg);
@@ -55,6 +85,7 @@ var Controller = function ($ionicActionSheet, $scope, XiaJia, localStorageServic
         });
     }
 
+    $scope._shangjiaWithKuwei = _shangjiaWithKuwei;
     $scope.off = function (item) {
         item.txt = '上架中';
         XiaJia.shangjia('?shiftId=' + item.id + '&userName=' + $scope.loginUser.loginNme + '&epsSupplyId=' + item.id).then(function (kuweis) {
@@ -114,4 +145,4 @@ var Controller = function ($ionicActionSheet, $scope, XiaJia, localStorageServic
 
   };
 
-module.exports = ['$ionicActionSheet', '$scope', 'XiaJia', 'localStorageService', '$state', '$ionicPopup', '$rootScope', '$interval', Controller];
+module.exports = ['$ionicModal', '$scope', 'XiaJia', 'localStorageService', '$state', '$ionicPopup', '$rootScope', '$interval', Controller];
